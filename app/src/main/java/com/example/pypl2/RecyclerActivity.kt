277518838bd016951f893105ce18cb7cd425e638
abdul <abdul.tanveer@gmail.com>
2024-09-13 11:48:51 +0530

    listview- arrayadapter

--- app/src/main/java/com/example/pypl2/RecyclerActivity.kt
+++ app/src/main/java/com/example/pypl2/RecyclerActivity.kt
@@ -1,6 +1,8 @@
 package com.example.pypl2
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,9 +10,18 @@ import androidx.core.view.WindowInsetsCompat
 
 class RecyclerActivity : AppCompatActivity() {
     var languages = arrayOf("english","hindi","tamil","telgu","kannada","urdu")
+
+    lateinit var  languagesListView: ListView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recycler)
+        languagesListView = findViewById(R.id.listView)
+
+        var myAdapter = ArrayAdapter(this,
+            android.R.layout.simple_list_item_1,
+            android.R.id.text1,
+            languages)
+        languagesListView.adapter = myAdapter
 
     }
 }
