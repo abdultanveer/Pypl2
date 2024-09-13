dd8dac8498ae2059183c0fc26c36dbe2ad05af54
abdul <abdul.tanveer@gmail.com>
2024-09-13 12:13:07 +0530

    RecyclerActivity

--- app/src/main/java/com/example/pypl2/RecyclerActivity.kt
+++ app/src/main/java/com/example/pypl2/RecyclerActivity.kt
@@ -1,27 +1,27 @@
 package com.example.pypl2
 
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.ListView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class RecyclerActivity : AppCompatActivity() {
     var languages = arrayOf("english","hindi","tamil","telgu","kannada","urdu")
 
-    lateinit var  languagesListView: ListView
+    lateinit var  languagesrecyView: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recycler)
-        languagesListView = findViewById(R.id.listView)
+        languagesrecyView = findViewById(R.id.recyclerView)
 
-        var myAdapter = ArrayAdapter(this,
-            android.R.layout.simple_list_item_1,
-            android.R.id.text1,
-            languages)
-        languagesListView.adapter = myAdapter
+//        var myAdapter = ArrayAdapter(this,
+//            android.R.layout.simple_list_item_1,
+//            android.R.id.text1,
+//            languages)
+
+        var myAdapter = LangsAdapter(languages)
+        languagesrecyView.layoutManager = LinearLayoutManager(this)
+        languagesrecyView.adapter = myAdapter
 
     }
 }
