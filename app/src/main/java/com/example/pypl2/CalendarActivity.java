cb8be86f4559ad680727992b346be16f75609581
abdul <abdul.tanveer@gmail.com>
2024-09-20 10:09:45 +0530

    conntent providers

--- app/src/main/java/com/example/pypl2/CalendarActivity.java
+++ app/src/main/java/com/example/pypl2/CalendarActivity.java
@@ -1,8 +1,12 @@
 package com.example.pypl2;
 
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -19,6 +23,22 @@ public class CalendarActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_calendar);
        
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        String[] from = new String[]{"body","address"};
+        int[] to = {android.R.id.text1,android.R.id.text2};
+        Uri uriSms = Uri.parse("content://sms/inbox");
+        Cursor cursor = getContentResolver().query(uriSms, null,null,null,null);
+        ListView contentListView = findViewById(R.id.listView);
+
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
+                android.R.layout.simple_list_item_2,cursor,from,to,0);
+        contentListView.setAdapter(adapter);
+
+
     }
 
     public void getSetTv(View view) {
